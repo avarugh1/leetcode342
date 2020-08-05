9ce4576591f921c370c5632c6cc2fac6b8e9b8ac
avarugh1 <avarugh1@users.noreply.github.com>
2020-08-04 22:55:36 -0400

    updated number of solutions

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,4 +1,49 @@
 public class Driver {
+    /**
+     * Solution 3: Utilizing Math.log for logical solution to if number is power of 4
+     * Java Math.pow return double which should be compared using epsilon
+     * @param num int input num
+     * @return boolean is power of 4
+     */
+    public boolean isPowerOfFour(int num) {
+        if(num < 1) return false;
+
+        int logBase4 = (int) (Math.log(num) / Math.log(4)); // reordering log base 4 of x
+        double result = Math.abs(Math.pow(4, logBase4) - num);
+        return (result < 1e-11); // comparison using episilon since Math.pow returns double
+    }
+
+    /**
+     * Solution 2: Bit Manipulation to determine if power of 2
+     * Based on if only 1 bit is set
+     * and if that bit is in an odd position
+     * @param num int input num
+     * @return boolean is power of 4
+     */
+    /*
+    public boolean isPowerOfFour(int num) {
+        int bitCount = Integer.bitCount(num);
+
+        if(bitCount == 1) { // powers of 4 will have only 1 bit set
+            int shifted = 0;
+            while (num > 0) {
+                num = num >> 1;
+                shifted++;
+            }
+            return ((shifted % 2) == 1); // number of shifts must be odd
+        }
+        return false;
+    }
+     */
+
+
+    /**
+     * Solution 1: Naive Solution to loop through dividing by 4
+     * and checking mod 4 to see if any remainder
+     * @param num input number
+     * @return boolean is power of 4
+     */
+    /*
     public boolean isPowerOfFour(int num) {
         // handling edge scenario
         if(num == 1) return true;
@@ -12,6 +57,7 @@ public class Driver {
         }
         return true;
     }
+    */
 
     public static void main(String[] args) {
         {
@@ -37,6 +83,16 @@ public class Driver {
         {
             Driver d = new Driver();
             boolean result = d.isPowerOfFour(17);
+            System.out.println(result); // false
+        }
+        {
+            Driver d = new Driver();
+            boolean result = d.isPowerOfFour(0);
+            System.out.println(result); // false
+        }
+        {
+            Driver d = new Driver();
+            boolean result = d.isPowerOfFour(-5);
             System.out.println(result); // false
         }
     }
